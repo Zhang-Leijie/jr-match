beca98cb59724fb8f136ca61e55ad234d0d17af5
Yuan Han <yuanhan1890@gmail.com>
2016-10-19 11:46:13 +0800

    breadcrumb, header-bar样式

--- match/vue/p2p/p2p-bid.vue
+++ match/vue/p2p/p2p-bid.vue
@@ -1,3 +1,11 @@
 <template>
-	<div>投标操作</div>
+	<div>
+		<div class="header-bar">
+			<ol class="breadcrumb">
+				<li class="item">P2P</li>
+				<li class="item">发标操作</li>
+			</ol>
+			<p class="remain-time">剩余时间：<span>30分00秒</span></p>
+		</div>
+	</div>
 </template>
